df0bf6eff627bc5c26be71fa395cbd69df7a7e62
Jomij <33017422+Jomij@users.noreply.github.com>
2017-10-24 15:10:06 +0530

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,26 @@
+from  sklearn.linear_model import LogisticRegression
+
+#from sklearn.neighbors import KNeighborsClassifier  #using KNN
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+features = iris.data
+labels = iris.target
+
+from sklearn.cross_validation import train_test_split  #fn train
+X_train, X_test, Y_train, Y_test = train_test_split(features,labels,test_size=.3)
+
+#neigh = KNeighborsClassifier()
+#neigh.fit(X_train,Y_train)
+#clf = DecisionTreeClassifier()
+#clf.fit(X_train,Y_train)
+#p = neigh.predict(X_test)
+clf = LogisticRegression()
+clf.fit(X_train,Y_train)
+p=clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print ("Accuracy =",accuracy_score(Y_test,p))
+
+
